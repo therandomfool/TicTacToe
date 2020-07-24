f76c0238c25f3afb32783407a63c9fe879bbf968
therandomfool <mddenver@protonmail.com>
2020-07-24 11:28:30 -0700

    gameboard and first turn complete

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 	
@@ -10,6 +12,24 @@ public class TicTacToe {
 		
 		printGameBoard(gameBoard);
 		
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter your placement from (1-9):");
+			int playerPos =scan.nextInt();
+			
+			
+			placePiece(gameBoard, playerPos, "player");
+			
+			Random rand = new Random();
+			int cpuPos = rand.nextInt(9)+1;
+			
+			placePiece(gameBoard, cpuPos, "cpu");
+			
+			printGameBoard(gameBoard);
+		}
+		
 	}
 	
 	public static void printGameBoard(char[][] gameBoard) {
@@ -19,6 +39,55 @@ public class TicTacToe {
 			}
 			System.out.println();
 		}
+	}
+	
+	public static void placePiece(char[][] gameBoard, int pos, String user) {
+		
+		char symbol = ' ';
+		
+		if(user.equals("player")) {
+			symbol = 'X';
+		} else if(user.equals("cpu") ) {
+			symbol = 'O';
+		}
+		switch(pos) {
+		case 1:
+			gameBoard[0][0] = symbol;
+			break;
+		case 2:
+			gameBoard[0][2] = symbol;
+			break;
+		case 3:
+			gameBoard[0][4] = symbol;
+			break;
+		case 4:
+			gameBoard[2][0] = symbol;
+			break;
+		case 5:
+			gameBoard[2][2] = symbol;
+			break;
+		case 6:
+			gameBoard[2][4] = symbol;
+			break;
+		case 7:
+			gameBoard[4][0] = symbol;
+			break;
+		case 8:
+			gameBoard[4][2] = symbol;
+			break;
+		case 9:
+			gameBoard[4][4] = symbol;
+			break;
+	}
+	}
+	
+	public static String checkWinner() {
+		
+		
+		
+		
+		return "";
+		
 	}
 
 }
